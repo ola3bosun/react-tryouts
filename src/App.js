e98b94f6818f98e5eecf6b08bc266b9d8f2d0f36
tubosun <olatubosundiyaolu@gmail.com>
2025-02-27 18:10:24 +0100

    centered grid to page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MagnetLines from './components/MagnetLine';
+import CircularText from './components/sokiosdare';
+
+
+function App() {
+  return (
+
+    <MagnetLines />
+  );
+}
+
+export default App;
